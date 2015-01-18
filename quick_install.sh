cb5e8a5df3623e2a312dedee2cf3bad39021008f
nbauma109 <nbauma109@users.noreply.github.com>
2015-01-18 18:41:51 +0100

    read pass from console

--- quick_install.sh
+++ quick_install.sh
@@ -9,8 +9,11 @@ else
 	yum install -y git
 fi
 
-git clone https://nbauma109@github.com/nbauma109/ibc
-git clone https://nbauma109@github.com/nbauma109/r2d2
+user='nbauma109'
+read -s pass
+
+git clone https://$user:$pass@github.com/$user/ibc
+git clone https://$user:$pass@github.com/$user/r2d2
 
 pushd ibc
 ./install.sh
