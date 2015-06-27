1f4f69894ba50327af6f731bddb5dd43251a603f
nbauma109 <nbauma109@users.noreply.github.com>
2015-06-27 20:29:51 +0200

    Update quick_install.bat

--- quick_install.bat
+++ quick_install.bat
@@ -6,7 +6,7 @@ cinst -y --force python2
 cinst -y --force phantomjs
 pushd c:\
 rd /s /q casperjs
-git clone https://github.com/n1k0/casperjs
+"%ProgramFiles(x86)%\Git\cmd\git" clone https://github.com/n1k0/casperjs
 popd
 if "%PATH%"=="%PATH:casperjs=%" (
     echo Adding casperjs to PATH 
