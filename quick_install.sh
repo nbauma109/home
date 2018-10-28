52b59afc46fec8ea7874307d613cff97d034a42d
nbauma109 <nbauma109@github>
2018-10-28 11:25:53 +0100

    runit fix

--- quick_install.sh
+++ quick_install.sh
@@ -24,8 +24,20 @@ git clone https://$user@github.com/$user/idsrd
 
 mkdir -p ~/Downloads
 
-./installRUNIT.sh
-echo
+#runit installation
+mkdir -p /package
+chmod 1755 /package
+pushd /package
+wget -qO- http://smarden.org/runit/install.html | grep -o runit-[0-9.]*tar.gz | head -n1 | xargs -I '{}' sh -c \
+'wget http://smarden.org/runit/{} -O {} && tar xzvf {}'
+pushd admin/runit*
+package/install
+package/install-man
+install -m0750 /package/admin/runit/etc/2 /sbin/runsvdir-start
+mkdir -p /service
+popd
+popd
+
 
 pushd r2d2
 ./install.sh
