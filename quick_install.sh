922c45ebac4bb160b57d116d360ed6af82a239ff
nbauma109 <nbauma109@github>
2018-10-28 11:05:13 +0100

    openjdk

--- quick_install.sh
+++ quick_install.sh
@@ -7,10 +7,10 @@ popd
 if [ -f /usr/bin/apt-get ]
 then
 	apt-get update
-	apt-get install -y wget curl git xvfb unzip libxrender1 libxtst6 libxi6 csh curl g++ make psmisc
+	apt-get install -y wget curl git xvfb unzip libxrender1 libxtst6 libxi6 csh curl g++ make psmisc default-jdk
 else
 	yum update -y
-	yum install -y wget curl git dbus-x11 Xvfb unzip libXrender libXtst libXi csh curl gcc-c++ glibc-static make psmisc
+	yum install -y wget curl git dbus-x11 Xvfb unzip libXrender libXtst libXi csh curl gcc-c++ glibc-static make psmisc java-1.8.0-openjdk-devel
 	dbus-uuidgen --ensure	
 fi
 
@@ -23,9 +23,6 @@ git clone https://$user@github.com/$user/r2d2
 git clone https://$user@github.com/$user/idsrd
 
 mkdir -p ~/Downloads
-
-./installJDK.sh
-echo
 
 ./installRUNIT.sh
 echo
