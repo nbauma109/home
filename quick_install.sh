d1389486264f5554fb1ba05b4e155214d8ad58ff
nbauma109 <nbauma109@users.noreply.github.com>
2018-12-02 20:16:46 +0100

    Update quick_install.sh

--- quick_install.sh
+++ quick_install.sh
@@ -7,7 +7,7 @@ popd
 if [ -f /usr/bin/apt-get ]
 then
 	apt-get update
-	apt-get install -y wget curl git xvfb unzip libxrender1 libxtst6 libxi6 libhyphen0 csh curl g++ make psmisc default-jdk poppler-utils python python-ipaddr bzip2 fontconfig lynx firefox
+	apt-get install -y wget curl git xvfb unzip libxrender1 libxtst6 libxi6 libhyphen0 csh curl g++ make psmisc default-jdk poppler-utils python python-ipaddr bzip2 fontconfig lynx firefox-esr
 else
 	yum update -y
 	yum install -y redhat-lsb-core wget curl git dbus-x11 Xvfb unzip libXrender libXtst libXi csh curl gcc-c++ glibc-static make psmisc java-1.8.0-openjdk-devel poppler-utils python python-ipaddr bzip2 fontconfig lynx firefox
